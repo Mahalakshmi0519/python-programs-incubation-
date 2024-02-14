52abc544ab395d22b0cea763a1f73e0ff975fba8
Mahalakshmi0519 <mahaunofficial123@gmail.com>
2024-02-14 16:53:43 +0530

    python-programs-incubation

---
+++ userinput.py
@@ -0,0 +1,7 @@
+''' user input data can be given dynamically while executing using predefined function input() i.e., varname = input()
+and it always takes the data in string form'''
+
+
+name =input('enter the name : ')
+print(type(name))
+print(name)
